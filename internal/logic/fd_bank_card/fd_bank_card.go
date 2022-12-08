7c7665ace0e736f2948ed9bc2e344b6ad38a7db4
LinFeiFei <2778368047@qq.com>
2022-12-08 12:00:56 +0800

    feat: 添加删除银行卡方法，优化添加银行卡方法逻辑
    - 添加银行卡：
    	- 通过银行卡号判断银行卡是否存在
    
    - 删除银行卡：(标记删除)
    	-  1.修改状态为禁用0
    	-  2. 标记银行卡号：bankcardNum_bankCard.Id的后六位

--- internal/logic/fd_bank_card/fd_bank_card.go
+++ internal/logic/fd_bank_card/fd_bank_card.go
@@ -9,6 +9,7 @@ import (
 	"github.com/SupenBysz/gf-admin-community/service"
 	"github.com/SupenBysz/gf-admin-community/utility/daoctl"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/text/gstr"
 	"github.com/gogf/gf/v2/util/gconv"
 	"github.com/yitter/idgenerator-go/idgen"
 	"time"
@@ -41,10 +42,18 @@ func (s *sFdBankCard) CreateBankCard(ctx context.Context, info model.BankCardReg
 		return nil, service.SysLogs().ErrorSimple(ctx, err, "用户信息查询失败,用户id错误", dao.SysUser.Table())
 	}
 
+	// 判断银行卡是否重复
+	bankCard, err := s.GetBankCardByCardNumber(ctx, info.CardNumber)
+
+	if bankCard != nil || err == nil {
+		return nil, service.SysLogs().ErrorSimple(ctx, err, "银行卡号已存在", dao.FdBankCard.Table())
+	}
+
 	bankCardInfo := do.FdBankCard{}
 	gconv.Struct(info, &bankCardInfo)
 	bankCardInfo.Id = idgen.NextId()
 
+	// 添加银行卡
 	_, err = dao.FdBankCard.Ctx(ctx).Data(bankCardInfo).Insert()
 
 	if err != nil {
@@ -90,10 +99,34 @@ func (s *sFdBankCard) UpdateBankCardState(ctx context.Context, bankCardId int64,
 	// 修改状态
 	result, err := dao.FdBankCard.Ctx(ctx).Where(do.FdBankCard{Id: bankCardId}).Update(do.FdBankCard{State: state})
 
-	//_, err = dao.SysUser.Ctx(ctx).Where(do.SysUser{Id: sysUserInfo.Id}).Update(do.SysUser{Password: pwdHash})
-
 	if err != nil || result == nil {
 		return false, service.SysLogs().ErrorSimple(ctx, err, "银行卡状态修改失败", dao.FdBankCard.Table())
+	}
+
+	return true, nil
+}
+
+// DeleteBankCardById 删除银行卡 (标记删除: 标记删除的银行卡号，将记录ID的后6位附加到卡号尾部，用下划线隔开,并修改状态)
+func (s *sFdBankCard) DeleteBankCardById(ctx context.Context, bankCardId int64) (bool, error) {
+	bankCard, err := s.GetBankCardById(ctx, bankCardId)
+	if err != nil || bankCard == nil {
+		return false, service.SysLogs().ErrorSimple(ctx, err, "银行卡不存在", dao.FdBankCard.Table())
+	}
+
+	bankCardIdLen := len(gconv.String(bankCard.Id))
+
+	subId := gstr.SubStr(gconv.String(bankCard.Id), bankCardIdLen-6, 6)
+
+	// 1.修改状态为禁用0   2. 标记银行卡号：bankcardNum_bankCard.Id的后六位
+	newBankCardNum := bankCard.CardNumber + "_" + subId
+
+	result, err := dao.FdBankCard.Ctx(ctx).Where(do.FdBankCard{Id: bankCardId}).Update(do.FdBankCard{
+		State:      0,
+		CardNumber: newBankCardNum,
+	})
+
+	if err != nil || result == nil {
+		return false, service.SysLogs().ErrorSimple(ctx, err, "银行卡删除失败", dao.FdBankCard.Table())
 	}
 
 	return true, nil
