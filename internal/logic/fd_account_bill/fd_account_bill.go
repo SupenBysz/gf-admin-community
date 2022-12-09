a78e0dac28f10d74d0f9759b4c9291590a801b7d
LinFeiFei <2778368047@qq.com>
2022-12-09 10:05:30 +0800

    pref: 优化财务账单的spending方法

--- internal/logic/fd_account_bill/fd_account_bill.go
+++ internal/logic/fd_account_bill/fd_account_bill.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/SupenBysz/gf-admin-community/model"
 	"github.com/SupenBysz/gf-admin-community/model/dao"
-	"github.com/SupenBysz/gf-admin-community/model/entity"
 	kyFinancial "github.com/SupenBysz/gf-admin-community/model/enum/financial"
 	"github.com/SupenBysz/gf-admin-community/service"
 	"github.com/gogf/gf/v2/database/gdb"
@@ -85,6 +84,7 @@ func (s *sFdAccountBill) income(ctx context.Context, info model.AccountBillRegis
 	if err != nil || toUser == nil {
 		return false, service.SysLogs().ErrorSimple(ctx, gerror.NewCode(gcode.CodeBusinessValidationFailed, "交易失败,交易接收者不存在"), "", dao.FdAccountBill.Table())
 	}
+
 	// 先通过财务账号id查询账号出来
 	account, err := service.FdAccount().GetAccountById(ctx, info.FdAccountId)
 
@@ -99,7 +99,7 @@ func (s *sFdAccountBill) income(ctx context.Context, info model.AccountBillRegis
 	err = dao.FdAccount.Ctx(ctx).Transaction(ctx, func(ctx context.Context, tx *gdb.TX) error {
 		// 版本
 		version := account.Version
-		// 余额 = 之前的余额 + 本次交易的余额
+		// 余额 = 之前的余额 + 本次交易的金额
 		afterBalance := account.Balance + info.Amount
 
 		// 1. 添加一条收入财务账单流水
@@ -149,48 +149,56 @@ func (s *sFdAccountBill) income(ctx context.Context, info model.AccountBillRegis
 
 // spending 支出
 func (s *sFdAccountBill) spending(ctx context.Context, info model.AccountBillRegister) (bool, error) {
+	// 先通过财务账号id查询账号出来
+	account, err := service.FdAccount().GetAccountById(ctx, info.FdAccountId)
+	if err != nil {
+		return false, service.SysLogs().ErrorSimple(ctx, gerror.NewCode(gcode.CodeBusinessValidationFailed, "交易失败,交易接收账户查询失败"), "", dao.FdAccountBill.Table())
+	}
+
+	bill := model.AccountBillInfo{}
 
 	// 使用乐观锁校验余额，和更新余额
-	err := dao.FdAccount.Ctx(ctx).Transaction(ctx, func(ctx context.Context, tx *gdb.TX) error {
-		// 修改财务账号余额，增加一条财务账单
-		i := 0
-		var affected int64
-		for i == 0 {
-			if affected == 0 { // 未成功
-				// 先通过财务账号id查询账号出来
-				account, _ := service.FdAccount().GetAccountById(ctx, info.FdAccountId)
-				version := account.Version
-				afterBalance := account.Balance - info.Amount
+	err = dao.FdAccount.Ctx(ctx).Transaction(ctx, func(ctx context.Context, tx *gdb.TX) error {
+		// 版本
+		version := account.Version
+		// 余额 = 之前的余额 - 本次交易的金额
+		afterBalance := account.Balance - info.Amount
 
-				// 判断余额是否足够
-				if account.Balance >= info.Amount { // 足够
+		// 判断余额是否足够
+		if account.Balance >= info.Amount { // 足够
+			// 1. 添加一条财务账单流水
+			info.BeforeBalance = account.Balance
+			info.AfterBalance = afterBalance
 
-					// 1. 添加一条财务账单流水
-					info.BeforeBalance = account.Balance
-					info.AfterBalance = afterBalance
+			gconv.Struct(info, &bill)
+			bill.Id = idgen.NextId()
 
-					bill := entity.FdAccountBill{}
-					gconv.Struct(info, &bill)
-					bill.Id = idgen.NextId()
+			result, err := dao.FdAccountBill.Ctx(ctx).Insert(bill)
 
-					result, err := dao.FdAccountBill.Ctx(ctx).Insert(bill)
-
-					if result == nil || err != nil {
-						return gerror.New("财务账单添加失败！")
-					}
-
-					// 2.修改财务账号的余额
-
-					// 参数：上下文, 财务账号id, 需要修改的钱数目, 查询到的版本, 收支类型
-					affected, err = service.FdAccount().UpdateAccountBalance(ctx, account.Id, info.Amount, version, info.InOutType)
-
-				} else {
-					return gerror.New("交易发起者的账户余额不足")
-				}
-
-			} else { // affected !=  0
-				return nil
+			if result == nil || err != nil {
+				return service.SysLogs().ErrorSimple(ctx, gerror.NewCode(gcode.CodeBusinessValidationFailed, "财务账单创建失败"), "", dao.FdAccountBill.Table())
 			}
+
+			// 2.修改财务账号的余额
+			// 参数：上下文, 财务账号id, 需要修改的钱数目, 查询到的版本, 收支类型
+			affected, err := service.FdAccount().UpdateAccountBalance(ctx, account.Id, info.Amount, version, info.InOutType)
+
+			if affected == 0 || err != nil {
+				return service.SysLogs().ErrorSimple(ctx, gerror.NewCode(gcode.CodeBusinessValidationFailed, "更新账户余额失败"), "", dao.FdAccountBill.Table())
+			}
+
+			// Hook调用
+			for _, hook := range s.hookArr {
+				// 判断收支类型
+				if hook.Key.InTransaction && hook.Key.InOutType == kyFinancial.InOutType.Out {
+					// 判断交易类型
+					if hook.Key.TradeType.Code()&info.TradeType == info.TradeType {
+						hook.Value(ctx, hook.Key, bill)
+					}
+				}
+			}
+		} else {
+			return gerror.New("交易发起者的账户余额不足")
 		}
 
 		return nil
@@ -198,6 +206,14 @@ func (s *sFdAccountBill) spending(ctx context.Context, info model.AccountBillReg
 
 	if err != nil {
 		return false, service.SysLogs().ErrorSimple(ctx, gerror.NewCode(gcode.CodeBusinessValidationFailed, "交易失败"), err.Error(), dao.FdAccountBill.Table())
+	}
+
+	for _, hook := range s.hookArr {
+		if !hook.Key.InTransaction && hook.Key.InOutType == kyFinancial.InOutType.Out {
+			if hook.Key.TradeType.Code()&info.TradeType == info.TradeType {
+				hook.Value(ctx, hook.Key, bill)
+			}
+		}
 	}
 
 	return true, nil
