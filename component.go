bdcedcd78fc6c554cf60b9e5e04e215e55127060
Supen.Huang <supen.huang@qq.com>
2022-12-14 10:41:51 +0800

    pref 优化启动流程

--- component.go
+++ component.go
@@ -1,15 +1,7 @@
 package component
 
 import (
-	"github.com/SupenBysz/gf-admin-community/utility/env"
-	"github.com/SupenBysz/gf-admin-community/utility/validator"
-
+	_ "github.com/SupenBysz/gf-admin-community/internal/boot"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic"
 	_ "github.com/gogf/gf/contrib/drivers/pgsql/v2"
 )
-
-func init() {
-	env.LoadEnv()
-	// 注册服务电话验证规则
-	validator.RegisterServicePhone()
-}
