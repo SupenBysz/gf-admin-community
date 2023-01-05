1c0f757901937c48bd4b93671890be99871b946b
0852FeiFeiLin <2778368047@qq.com>
2023-01-06 00:45:09 +0800

    feat: cootroller添加权限判断

--- sys_controller/sys_organization.go
+++ sys_controller/sys_organization.go
@@ -5,6 +5,7 @@ import (
 	"github.com/SupenBysz/gf-admin-community/api_v1"
 	"github.com/SupenBysz/gf-admin-community/api_v1/sys_api"
 	"github.com/SupenBysz/gf-admin-community/sys_model"
+	"github.com/SupenBysz/gf-admin-community/sys_model/sys_enum"
 	"github.com/SupenBysz/gf-admin-community/sys_service"
 	"github.com/gogf/gf/v2/util/gconv"
 )
@@ -16,11 +17,21 @@ type cSysOrganization struct{}
 
 // QueryOrganizationList 获取组织架构|列表
 func (c *cSysOrganization) QueryOrganizationList(ctx context.Context, req *sys_api.QueryOrganizationListReq) (*sys_model.OrganizationInfoListRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.List); err != nil {
+		return nil, err
+	}
+
 	return sys_service.SysOrganization().QueryOrganizationList(ctx, req.SearchParams)
 }
 
 // GetOrganizationList 根据ID获取下级组织架构|列表
 func (c *cSysOrganization) GetOrganizationList(ctx context.Context, req *sys_api.GetOrganizationListReq) (*sys_model.OrganizationInfoListRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.List); err != nil {
+		return nil, err
+	}
+
 	data, count, err := sys_service.SysOrganization().GetOrganizationList(ctx, req.Id, req.IsRecursive)
 	if err != nil {
 		return nil, err
@@ -41,6 +52,11 @@ func (c *cSysOrganization) GetOrganizationList(ctx context.Context, req *sys_api
 
 // GetOrganizationTree 根据ID获取下级组织架构|树
 func (c *cSysOrganization) GetOrganizationTree(ctx context.Context, req *sys_api.GetOrganizationTreeReq) (*sys_api.OrganizationInfoTreeListRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.List); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysOrganization().GetOrganizationTree(ctx, req.Id)
 
 	return (*sys_api.OrganizationInfoTreeListRes)(result), err
@@ -48,18 +64,33 @@ func (c *cSysOrganization) GetOrganizationTree(ctx context.Context, req *sys_api
 
 // CreateOrganizationInfo 创建或更新组织架构|信息
 func (c *cSysOrganization) CreateOrganizationInfo(ctx context.Context, req *sys_api.CreateOrganizationInfoReq) (*sys_api.OrganizationInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.Create); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysOrganization().CreateOrganizationInfo(ctx, req.SysOrganizationInfo)
 	return (*sys_api.OrganizationInfoRes)(result), err
 }
 
 // UpdateOrganizationInfo 创建或更新组织架构|信息
 func (c *cSysOrganization) UpdateOrganizationInfo(ctx context.Context, req *sys_api.UpdateOrganizationInfoReq) (*sys_api.OrganizationInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.Update); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysOrganization().UpdateOrganizationInfo(ctx, req.SysOrganizationInfo)
 	return (*sys_api.OrganizationInfoRes)(result), err
 }
 
 // GetOrganizationInfo 获取组织架构|信息
 func (c *cSysOrganization) GetOrganizationInfo(ctx context.Context, req *sys_api.GetOrganizationInfoReq) (*sys_api.OrganizationInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.View); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysOrganization().GetOrganizationInfo(ctx, req.Id)
 
 	return (*sys_api.OrganizationInfoRes)(result), err
@@ -67,6 +98,11 @@ func (c *cSysOrganization) GetOrganizationInfo(ctx context.Context, req *sys_api
 
 // DeleteOrganizationInfo 根据ID删除组织架构
 func (c *cSysOrganization) DeleteOrganizationInfo(ctx context.Context, req *sys_api.DeleteOrganizationInfoReq) (api_v1.BoolRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Organization.PermissionType.Delete); err != nil {
+		return false, err
+	}
+
 	result, err := sys_service.SysOrganization().DeleteOrganizationInfo(ctx, req.Id)
 
 	return result == true, err
--- sys_controller/sys_permission.go
+++ sys_controller/sys_permission.go
@@ -5,6 +5,7 @@ import (
 	"github.com/SupenBysz/gf-admin-community/api_v1"
 	"github.com/SupenBysz/gf-admin-community/api_v1/sys_api"
 	"github.com/SupenBysz/gf-admin-community/sys_model"
+	"github.com/SupenBysz/gf-admin-community/sys_model/sys_enum"
 	"github.com/SupenBysz/gf-admin-community/sys_service"
 )
 
@@ -15,18 +16,33 @@ type cSysPermission struct{}
 
 // GetPermissionById 根据权限ID获取权限信|息
 func (c *cSysPermission) GetPermissionById(ctx context.Context, req *sys_api.GetPermissionByIdReq) (*sys_model.SysPermissionInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.View); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysPermission().GetPermissionById(ctx, req.Id)
 	return (*sys_model.SysPermissionInfoRes)(result), err
 }
 
 // GetPermissionByName 根据权限Name获取权限|信息
 func (c *cSysPermission) GetPermissionByName(ctx context.Context, req *sys_api.GetPermissionByNameReq) (*sys_model.SysPermissionInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.View); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysPermission().GetPermissionByName(ctx, req.Name)
 	return (*sys_model.SysPermissionInfoRes)(result), err
 }
 
 // QueryPermissionListReq 查询权限|列表
 func (c *cSysPermission) QueryPermissionListReq(ctx context.Context, req *sys_api.QueryPermissionListReq) (*sys_model.SysPermissionInfoListRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.List); err != nil {
+		return nil, err
+	}
+
 	return sys_service.SysPermission().QueryPermissionList(ctx, req.SearchParams)
 }
 
@@ -53,24 +69,44 @@ func (c *cSysPermission) QueryPermissionListReq(ctx context.Context, req *sys_ap
 
 // GetPermissionTree 根据ID获取下级权限|树
 func (c *cSysPermission) GetPermissionTree(ctx context.Context, req *sys_api.GetPermissionTreeReq) (*sys_model.SysPermissionInfoTreeRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.List); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysPermission().GetPermissionTree(ctx, req.Id)
 	return (*sys_model.SysPermissionInfoTreeRes)(&result), err
 }
 
 // CreatePermission 新增权限|信息
 func (c *cSysPermission) CreatePermission(ctx context.Context, req *sys_api.CreatePermissionReq) (*sys_model.SysPermissionInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.Create); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysPermission().CreatePermission(ctx, req.SysPermission)
 	return (*sys_model.SysPermissionInfoRes)(result), err
 }
 
 // UpdatePermission 更新权限|信息
 func (c *cSysPermission) UpdatePermission(ctx context.Context, req *sys_api.UpdatePermissionReq) (*sys_model.SysPermissionInfoRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.Update); err != nil {
+		return nil, err
+	}
+
 	result, err := sys_service.SysPermission().UpdatePermission(ctx, req.SysPermission)
 	return (*sys_model.SysPermissionInfoRes)(result), err
 }
 
 // DeletePermission 删除权限
 func (c *cSysPermission) DeletePermission(ctx context.Context, req *sys_api.DeletePermissionReq) (api_v1.BoolRes, error) {
+	// 权限判断
+	if _, err := sys_service.SysPermission().CheckPermission(ctx, sys_enum.Permissions.PermissionType.Delete); err != nil {
+		return false, err
+	}
+
 	result, err := sys_service.SysPermission().DeletePermission(ctx, req.Id)
 	return result == true, err
 }
