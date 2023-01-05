c46588ce0fb5e40a797079f6a159bc8b716cb2b0
0852FeiFeiLin <2778368047@qq.com>
2023-01-05 22:43:15 +0800

    feat: 初始化权限树导入添加角色、权限、组织架构管理权限树

--- sys_consts/global.go
+++ sys_consts/global.go
@@ -23,6 +23,7 @@ var (
 	}
 	// PermissionTree 权限信息定义
 	PermissionTree = []*permission.SysPermissionTree{
+		// 用户管理权限树
 		{
 			SysPermission: &sys_entity.SysPermission{
 				Id:         5947106208184773,
@@ -41,6 +42,59 @@ var (
 				sys_enum.User.PermissionType.SetState,
 				// 修改密码，修改自己的登录密码
 				sys_enum.User.PermissionType.ChangePassword,
+			},
+		},
+		// 组织架构权限树
+		{
+			SysPermission: &sys_entity.SysPermission{
+				Id:         5948649344204869,
+				Name:       "组织架构",
+				Identifier: "Organization",
+				Type:       1,
+			},
+			Children: []*permission.SysPermissionTree{
+				// 查看
+				sys_enum.Organization.PermissionType.View,
+				// 查看列表
+				sys_enum.Organization.PermissionType.List,
+				// 更新
+				sys_enum.Organization.PermissionType.Update,
+				// 删除
+				sys_enum.Organization.PermissionType.Delete,
+				// 创建
+				sys_enum.Organization.PermissionType.Create,
+			},
+		},
+		// 角色管理权限树
+		{
+			SysPermission: &sys_entity.SysPermission{
+				Id:         5948684761759818,
+				Name:       "角色管理",
+				Identifier: "Role",
+				Type:       1,
+			},
+			Children: []*permission.SysPermissionTree{
+				sys_enum.Role.PermissionType.View,
+				sys_enum.Role.PermissionType.List,
+				sys_enum.Role.PermissionType.Update,
+				sys_enum.Role.PermissionType.Delete,
+				sys_enum.Role.PermissionType.Create,
+			},
+		},
+		// 权限管理权限树
+		{
+			SysPermission: &sys_entity.SysPermission{
+				Id:         5948684761759818,
+				Name:       "权限管理",
+				Identifier: "Permission",
+				Type:       1,
+			},
+			Children: []*permission.SysPermissionTree{
+				sys_enum.Permissions.PermissionType.View,
+				sys_enum.Permissions.PermissionType.List,
+				sys_enum.Permissions.PermissionType.Update,
+				sys_enum.Permissions.PermissionType.Delete,
+				sys_enum.Permissions.PermissionType.Create,
 			},
 		},
 	}
