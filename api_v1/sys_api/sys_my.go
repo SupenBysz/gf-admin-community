33942161225807444ab3faf991f0dbf688d6ad8f
0852FeiFeiLin <2778368047@qq.com>
2023-01-05 22:44:17 +0800

    feat: 接口url绑定权限id
    - 通过中间件获取cp值进行具体的权限判断

--- api_v1/sys_api/sys_my.go
+++ api_v1/sys_api/sys_my.go
@@ -6,11 +6,11 @@ import (
 )
 
 type SetUsernameByIdReq struct {
-	g.Meta      `path:"/setUsername" method:"post" summary:"设置用户登陆名称" tags:"我的"`
+	g.Meta      `path:"/setUsername?cp=5949854362632262" method:"post" summary:"设置用户登陆名称" tags:"我的"`
 	NewUsername string `json:"newUsername" v:"required#新用户名称" dc:"新的用户名称"`
 }
 
 type UpdateUserPasswordReq struct {
-	g.Meta `path:"/updateUserPassword" method:"post" summary:"修改用户密码" tags:"我的"`
+	g.Meta `path:"/updateUserPassword?cp=5947177469213125" method:"post" summary:"修改用户密码" tags:"我的"`
 	sys_model.UpdateUserPassword
 }
