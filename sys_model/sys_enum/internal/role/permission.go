6e57ab1c1b4ae2c90f2f8f3ce4b589992c5364f8
Supen.Huang <supen.huang@qq.com>
2023-01-06 00:30:39 +0800

    pref 优化权限信息

--- sys_model/sys_enum/internal/role/permission.go
+++ sys_model/sys_enum/internal/role/permission.go
@@ -5,19 +5,21 @@ import (
 )
 
 type permissionType struct {
-	View    *permission.SysPermissionTree
-	List    *permission.SysPermissionTree
-	Update  *permission.SysPermissionTree
-	Delete  *permission.SysPermissionTree
-	Create  *permission.SysPermissionTree
-	SetUser *permission.SysPermissionTree
+	View          *permission.SysPermissionTree
+	List          *permission.SysPermissionTree
+	Update        *permission.SysPermissionTree
+	Delete        *permission.SysPermissionTree
+	Create        *permission.SysPermissionTree
+	SetMember     *permission.SysPermissionTree
+	SetPermission *permission.SysPermissionTree
 }
 
 var PermissionType = permissionType{
-	View:    permission.New(5948684761759813, "View", "查看角色", "查看某个角色"),
-	List:    permission.New(5948684761759814, "List", "角色列表", "查看所有角色"),
-	Update:  permission.New(5948684761759815, "Update", "更新角色信息", "更新某个角色信息"),
-	Delete:  permission.New(5948684761759816, "Delete", "删除角色", "删除某个角色"),
-	Create:  permission.New(5948684761759817, "Create", "创建角色", "创建角色"),
-	SetUser: permission.New(5949854362632263, "SetUser", "设置角色用户", "设置某一个角色的用户"),
+	View:          permission.New(5948684761759813, "View", "查看角色", "查看某个角色"),
+	List:          permission.New(5948684761759814, "List", "角色列表", "查看所有角色"),
+	Update:        permission.New(5948684761759815, "Update", "更新角色信息", "更新某个角色信息"),
+	Delete:        permission.New(5948684761759816, "Delete", "删除角色", "删除某个角色"),
+	Create:        permission.New(5948684761759817, "Create", "创建角色", "创建一个新角色"),
+	SetMember:     permission.New(5950451522795973, "SetMember", "设置角色成员", "增加或移除角色成员"),
+	SetPermission: permission.New(5950452043151813, "SetPermission", "设置角色权限", "设置某个角色的权限"),
 }
