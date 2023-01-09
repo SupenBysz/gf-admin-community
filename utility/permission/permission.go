04f43e7a89337a05a3316512ca542eb7f5789ace
Supen.Huang <supen.huang@qq.com>
2023-01-09 19:36:01 +0800

    Update permission.go
    pref 增加以标识符创建权限信息

--- utility/permission/permission.go
+++ utility/permission/permission.go
@@ -2,6 +2,7 @@ package permission
 
 import (
 	"github.com/SupenBysz/gf-admin-community/sys_model/sys_entity"
+	"github.com/yitter/idgenerator-go/idgen"
 )
 
 type SysPermissionTree struct {
@@ -24,6 +25,26 @@ func New(id int64, identifier string, name string, description ...string) *SysPe
 			Description: desc,
 			Identifier:  identifier,
 			Type:        1,
+		},
+	}
+}
+
+// NewInIdentifier 构造权限信息
+func NewInIdentifier(identifier string, name string, description ...string) *SysPermissionTree {
+	var desc string
+
+	if len(description) > 0 {
+		desc = description[0]
+	}
+
+	return &SysPermissionTree{
+		SysPermission: &sys_entity.SysPermission{
+			Id:          idgen.NextId(),
+			Name:        name,
+			Description: desc,
+			Identifier:  identifier,
+			Type:        1,
+			MatchMode:   1,
 		},
 	}
 }
