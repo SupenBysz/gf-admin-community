f7318c758cc93516ee76d93b0375b91511f0d3b2
Supen.Huang <supen.huang@qq.com>
2023-01-13 01:40:42 +0800

    chore 升级gf至2.3.0，并对应适配更新dao对象模型
    gf 工具需要升级至2.3.0

--- sys_model/sys_dao/internal/sys_menu.go
+++ sys_model/sys_dao/internal/sys_menu.go
@@ -92,6 +92,6 @@ func (dao *SysMenuDao) Ctx(ctx context.Context) *gdb.Model {
 //
 // Note that, you should not Commit or Rollback the transaction in function f
 // as it is automatically handled by this function.
-func (dao *SysMenuDao) Transaction(ctx context.Context, f func(ctx context.Context, tx *gdb.TX) error) (err error) {
+func (dao *SysMenuDao) Transaction(ctx context.Context, f func(ctx context.Context, tx gdb.TX) error) (err error) {
 	return dao.Ctx(ctx).Transaction(ctx, f)
 }
