1c21b5f7e6791b2685d57e7f6dd4bf4decc7896e
0852FeiFeiLin <2778368047@qq.com>
2023-01-14 16:18:15 +0800

    fix: 修复daoctl发现的问题
    - 数据库驱动不支持LastIndexId方法，去掉调用代码
    - data可选参数加上data... 避免获取不到值

--- utility/daoctl/insert.go
+++ utility/daoctl/insert.go
@@ -2,68 +2,58 @@ package daoctl
 
 import "github.com/gogf/gf/v2/database/gdb"
 
-func Insert(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64) {
-	result, err := model.Insert(data)
+func Insert(model *gdb.Model, data ...interface{}) (rowsAffected int64) {
+	result, err := model.Insert(data...)
 
 	if err != nil {
-		return 0, 0
+		return 0
 	}
 
 	rowsAffected, err = result.RowsAffected()
-	lastInsertId, err = result.LastInsertId()
 
-	return lastInsertId, rowsAffected
+	return rowsAffected
 }
 
-func InsertWithError(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64, err error) {
-	result, err := model.Insert(data)
+func InsertWithError(model *gdb.Model, data ...interface{}) (rowsAffected int64, err error) {
+	result, err := model.Insert(data...)
 
 	if err != nil {
-		return 0, 0, err
+		return 0, err
 	}
 
 	rowsAffected, err1 := result.RowsAffected()
-	lastInsertId, err2 := result.LastInsertId()
 
 	if err == nil && err1 != nil {
 		err = err1
 	}
-	if err == nil && err2 != nil {
-		err = err2
-	}
 
-	return lastInsertId, rowsAffected, err
+	return rowsAffected, err
 }
 
-func InsertIgnore(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64) {
-	result, err := model.InsertIgnore(data)
+func InsertIgnore(model *gdb.Model, data ...interface{}) (rowsAffected int64) {
+	result, err := model.InsertIgnore(data...)
 
 	if err != nil {
-		return 0, 0
+		return 0
 	}
 
 	rowsAffected, err = result.RowsAffected()
-	lastInsertId, err = result.LastInsertId()
 
-	return lastInsertId, rowsAffected
+	return rowsAffected
 }
 
-func InsertIgnoreWithError(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64, err error) {
-	result, err := model.Insert(data)
+func InsertIgnoreWithError(model *gdb.Model, data ...interface{}) (rowsAffected int64, err error) {
+	result, err := model.Insert(data...)
 
 	if err != nil {
-		return 0, 0, err
+		return 0, err
 	}
 
 	rowsAffected, err1 := result.RowsAffected()
-	lastInsertId, err2 := result.LastInsertId()
 
 	if err == nil && err1 != nil {
 		err = err1
 	}
-	if err == nil && err2 != nil {
-		err = err2
-	}
 
-	return lastInsertId, rowsAffected, err
+	return rowsAffected, err
 }
--- utility/daoctl/save.go
+++ utility/daoctl/save.go
@@ -4,35 +4,30 @@ import (
 	"github.com/gogf/gf/v2/database/gdb"
 )
 
-func Save(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64) {
-	result, err := model.Save(data)
+func Save(model *gdb.Model, data ...interface{}) (rowsAffected int64) {
+	result, err := model.Save(data...)
 
 	if err != nil {
-		return 0, 0
+		return 0
 	}
 
 	rowsAffected, err = result.RowsAffected()
-	lastInsertId, err = result.LastInsertId()
 
-	return lastInsertId, rowsAffected
+	return rowsAffected
 }
 
-func SaveWithError(model *gdb.Model, data ...interface{}) (lastInsertId int64, rowsAffected int64, err error) {
-	result, err := model.Save(data)
+func SaveWithError(model *gdb.Model, data ...interface{}) (rowsAffected int64, err error) {
+	result, err := model.Save(data...)
 
 	if err != nil {
-		return 0, 0, err
+		return 0, err
 	}
 
 	rowsAffected, err1 := result.RowsAffected()
-	lastInsertId, err2 := result.LastInsertId()
 
 	if err == nil && err1 != nil {
 		err = err1
 	}
-	if err == nil && err2 != nil {
-		err = err2
-	}
 
-	return lastInsertId, rowsAffected, err
+	return rowsAffected, err
 }
