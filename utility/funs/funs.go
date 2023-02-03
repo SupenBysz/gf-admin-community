bcf9e0c4622d1233b8bbff505b5438e1fb618e77
0852FeiFeiLin <2778368047@qq.com>
2023-02-03 16:41:38 +0800

    feat: 添加主体资质需要用到的Check函数至funs
    - CheckLicenseFiles 检查主体资质文件

--- utility/funs/funs.go
+++ utility/funs/funs.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/SupenBysz/gf-admin-community/sys_model"
+	"github.com/SupenBysz/gf-admin-community/sys_model/sys_do"
+	"github.com/SupenBysz/gf-admin-community/sys_model/sys_entity"
 	"github.com/SupenBysz/gf-admin-community/sys_service"
 	"github.com/SupenBysz/gf-admin-community/utility/permission"
+	"github.com/gogf/gf/v2/os/gfile"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 func If[R any](condition bool, trueVal, falseVal R) R {
@@ -45,6 +50,77 @@ func FilterUnionMain(ctx context.Context, search *sys_model.SearchParams, unionM
 	search.Filter = newFilter
 
 	return search
+}
+
+func CheckLicenseFiles[T sys_entity.SysLicense | sys_do.SysLicense](ctx context.Context, info sys_model.License, data *T) (response *T, err error) {
+	newData := &sys_entity.SysLicense{}
+	gconv.Struct(data, newData)
+
+	{
+		userId := sys_service.SysSession().Get(ctx).JwtClaimsUser.Id
+
+		// 用户资源文件夹
+		userFolder := "./resources/license/" + gconv.String(newData.Id)
+		fileAt := gtime.Now().Format("YmdHis")
+		if !gfile.Exists(info.IdcardFrontPath) {
+			// 检测缓存文件
+			fileInfoCache, err := sys_service.File().GetUploadFile(ctx, gconv.Int64(info.IdcardFrontPath), userId, "请上传身份证头像面")
+			if err != nil {
+				return nil, err
+			}
+			// 保存员工身份证头像面
+			fileInfo, err := sys_service.File().SaveFile(ctx, userFolder+"/idCard/front_"+fileAt+fileInfoCache.Ext, fileInfoCache)
+			if err != nil {
+				return nil, err
+			}
+			newData.IdcardFrontPath = fileInfo.Src
+		}
+
+		if !gfile.Exists(info.IdcardBackPath) {
+			// 检测缓存文件
+			fileInfoCache, err := sys_service.File().GetUploadFile(ctx, gconv.Int64(info.IdcardBackPath), userId, "请上传身份证国徽面")
+			if err != nil {
+				return nil, err
+			}
+			// 保存员工身份证国徽面
+			fileInfo, err := sys_service.File().SaveFile(ctx, userFolder+"/idCard/back_"+fileAt+fileInfoCache.Ext, fileInfoCache)
+			if err != nil {
+				return nil, err
+			}
+			newData.IdcardBackPath = fileInfo.Src
+		}
+
+		if !gfile.Exists(info.BusinessLicensePath) {
+			// 检测缓存文件
+			fileInfoCache, err := sys_service.File().GetUploadFile(ctx, gconv.Int64(info.BusinessLicensePath), userId, "请上传营业执照图片")
+			if err != nil {
+				return nil, err
+			}
+			// 保存营业执照图片
+			fileInfo, err := sys_service.File().SaveFile(ctx, userFolder+"/businessLicense/"+fileAt+fileInfoCache.Ext, fileInfoCache)
+			if err != nil {
+				return nil, err
+			}
+			newData.BusinessLicensePath = fileInfo.Src
+		}
+
+		if info.BusinessLicenseLegalPath != "" && !gfile.Exists(info.BusinessLicenseLegalPath) {
+			// 检测缓存文件
+			fileInfoCache, err := sys_service.File().GetUploadFile(ctx, gconv.Int64(info.BusinessLicenseLegalPath), userId, "请上传营业执照图片")
+			if err != nil {
+				return nil, err
+			}
+			// 保存法人单位营业执照图片
+			fileInfo, err := sys_service.File().SaveFile(ctx, userFolder+"/businessLicense/"+fileAt+fileInfoCache.Ext, fileInfoCache)
+			if err != nil {
+				return nil, err
+			}
+			newData.BusinessLicenseLegalPath = fileInfo.Src
+		}
+	}
+
+	gconv.Struct(newData, data)
+	return data, err
 }
 
 func CheckPermission[TRes any](ctx context.Context, f func() (TRes, error), permissions ...*permission.SysPermissionTree) (TRes, error) {
