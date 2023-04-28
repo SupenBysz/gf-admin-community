9c169545fe152f5aadecc73023783b49e95dd464
0852FeiFeiLin <2778368047@qq.com>
2023-04-28 14:56:17 +0800

    pref: 优化model模型字段描述

--- sys_model/sys_license.go
+++ sys_model/sys_license.go
@@ -7,32 +7,32 @@ import (
 )
 
 type License struct {
-	IdcardFrontPath           string      `json:"idcardFrontPath"           description:"身份证头像面照片" v:"required-license#请上传身份证头像面照片"`
-	IdcardBackPath            string      `json:"idcardBackPath"            description:"身份证国徽面照片" v:"required-license#请上传身份证国徽面照片"`
-	IdcardNo                  string      `json:"idcardNo"                  description:"身份证号" v:"required-license|resident-id#请输入身份证号|请输入正确的身份证号"`
-	IdcardExpiredDate         *gtime.Time `json:"idcardExpiredDate"         description:"身份证有效期" v:"date#身份证日期格式错误"`
-	IdcardAddress             string      `json:"idcardAddress"             description:"身份证户籍地址" v:"max-length:128#身份证地址最大支持128个字符"`
-	PersonContactName         string      `json:"personContactName"         description:"法人，必须是自然人" v:"required-license|max-length:16#请输入法人姓名|法人姓名最大支持16个字符"`
-	PersonContactMobile       string      `json:"personContactMobile"       description:"法人，联系电话" v:"required-license|max-length:16#请输入法人联系电话|法人联系电话最大支持16个字符"`
-	BusinessLicenseName       string      `json:"businessLicenseName"       description:"公司全称" v:"required-license|max-length:128#请输入公司全称|公司全称最大支持128个字符"`
-	BusinessLicenseAddress    string      `json:"businessLicenseAddress"    description:"公司地址" v:"required-license|max-length:128#请输入公司地址|公司地址最大支持128个字符"`
-	BusinessLicensePath       string      `json:"businessLicensePath"       description:"营业执照图片地址" v:"required-license#请上传营业执照图片"`
-	BusinessLicenseScope      string      `json:"businessLicenseScope"      description:"经营范围"`
-	BusinessLicenseRegCapital string      `json:"businessLicenseRegCapital" description:"注册资本" v:"max-length:32#注册资本最大支持32字符"`
-	BusinessLicenseTermTime   string      `json:"businessLicenseTermTime"   description:"营业期限" v:"max-length:64#营业期限最大支持64字符"`
-	BusinessLicenseOrgCode    string      `json:"businessLicenseOrgCode"    description:"组织机构代码" v:"max-length:16#组织机构代码最大支持16字符"`
-	BusinessLicenseCreditCode string      `json:"businessLicenseCreditCode" description:"统一社会信用代码" v:"required-license|max-length:32#请输入统一社会信用代码|统一社会信用代码最大支持32个字符"`
-	BusinessLicenseLegal      string      `json:"businessLicenseLegal"      description:"法人" v:"required-license|max-length:64#请输入法人名称|法人名称最大支持64个字符"`
-	BusinessLicenseLegalPath  string      `json:"businessLicenseLegalPath"  description:"法人证照，如果法人不是自然人，则该项必填" v:"max-length:256#法人证照地址最大支持256个字符"`
-	Remake                    string      `json:"remake"                    description:"备注"`
+	IdcardFrontPath           string      `json:"idcardFrontPath"           dc:"身份证头像面照片" v:"required-license#请上传身份证头像面照片"`
+	IdcardBackPath            string      `json:"idcardBackPath"            dc:"身份证国徽面照片" v:"required-license#请上传身份证国徽面照片"`
+	IdcardNo                  string      `json:"idcardNo"                  dc:"身份证号" v:"required-license|resident-id#请输入身份证号|请输入正确的身份证号"`
+	IdcardExpiredDate         *gtime.Time `json:"idcardExpiredDate"         dc:"身份证有效期" v:"date#身份证日期格式错误"`
+	IdcardAddress             string      `json:"idcardAddress"             dc:"身份证户籍地址" v:"max-length:128#身份证地址最大支持128个字符"`
+	PersonContactName         string      `json:"personContactName"         dc:"法人，必须是自然人" v:"required-license|max-length:16#请输入法人姓名|法人姓名最大支持16个字符"`
+	PersonContactMobile       string      `json:"personContactMobile"       dc:"法人，联系电话" v:"required-license|max-length:16#请输入法人联系电话|法人联系电话最大支持16个字符"`
+	BusinessLicenseName       string      `json:"businessLicenseName"       dc:"公司全称" v:"required-license|max-length:128#请输入公司全称|公司全称最大支持128个字符"`
+	BusinessLicenseAddress    string      `json:"businessLicenseAddress"    dc:"公司地址" v:"required-license|max-length:128#请输入公司地址|公司地址最大支持128个字符"`
+	BusinessLicensePath       string      `json:"businessLicensePath"       dc:"营业执照图片地址" v:"required-license#请上传营业执照图片"`
+	BusinessLicenseScope      string      `json:"businessLicenseScope"      dc:"经营范围"`
+	BusinessLicenseRegCapital string      `json:"businessLicenseRegCapital" dc:"注册资本" v:"max-length:32#注册资本最大支持32字符"`
+	BusinessLicenseTermTime   string      `json:"businessLicenseTermTime"   dc:"营业期限" v:"max-length:64#营业期限最大支持64字符"`
+	BusinessLicenseOrgCode    string      `json:"businessLicenseOrgCode"    dc:"组织机构代码" v:"max-length:16#组织机构代码最大支持16字符"`
+	BusinessLicenseCreditCode string      `json:"businessLicenseCreditCode" dc:"统一社会信用代码" v:"required-license|max-length:32#请输入统一社会信用代码|统一社会信用代码最大支持32个字符"`
+	BusinessLicenseLegal      string      `json:"businessLicenseLegal"      dc:"法人" v:"required-license|max-length:64#请输入法人名称|法人名称最大支持64个字符"`
+	BusinessLicenseLegalPath  string      `json:"businessLicenseLegalPath"  dc:"法人证照，如果法人不是自然人，则该项必填" v:"max-length:256#法人证照地址最大支持256个字符"`
+	Remake                    string      `json:"remake"                    dc:"备注"`
 }
 
 type LicenseRes sys_entity.SysLicense
 type LicenseListRes base_model.CollectRes[sys_entity.SysLicense]
 
 type AuditLicense struct {
-	UnionMainId int64 `json:"unionMainId"             description:"资质审核关联的业务主体ID"`
-	LicenseId   int64 `json:"licenseId"             description:"资质ID"`
+	UnionMainId int64 `json:"unionMainId"             dc:"资质审核关联的业务主体ID"`
+	LicenseId   int64 `json:"licenseId"             dc:"资质ID"`
 	UserId      int64 `json:"userId" dc:"上传资质的userId"`
 	License
 }
--- sys_model/sys_setting.go
+++ sys_model/sys_setting.go
@@ -6,10 +6,10 @@ import (
 )
 
 type SysSettings struct {
-	Name        string `json:"name"        description:"配置名称"`
-	Values      string `json:"values"      description:"配置信息JSON格式"`
-	Desc        string `json:"desc"        description:"描述"`
-	UnionMainId int64  `json:"unionMainId" description:"关联的主体id，为0代表是平台配置"`
+	Name        string `json:"name"        dc:"配置名称"`
+	Values      string `json:"values"      dc:"配置信息JSON格式"`
+	Desc        string `json:"desc"        dc:"描述"`
+	UnionMainId int64  `json:"unionMainId" dc:"关联的主体id，为0代表是平台配置"`
 }
 
 type SysSettingsRes sys_entity.SysSettings
