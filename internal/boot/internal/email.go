c57d89e70d94c1d3e23b60a7d4048b8826670159
Supen.Huang <supen.huang@qq.com>
2023-07-24 07:49:49 +0800

    pref: 优化Boot 代码结构

---
+++ internal/boot/internal/email.go
@@ -0,0 +1,29 @@
+package internal
+
+import (
+	"context"
+	"github.com/SupenBysz/gf-admin-community/sys_consts"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/glog"
+)
+
+func InitEmail() {
+	emailVar, err := g.Cfg().Get(context.Background(), "email")
+
+	if err != nil {
+		glog.Error(context.Background(), "加载邮件配置信息失败")
+	}
+
+	if emailVar == nil && err == nil {
+		glog.Warning(context.Background(), "邮件配置信息未设置")
+		return
+	}
+
+	err = emailVar.Struct(&sys_consts.Global.EmailConfig)
+
+	if err != nil {
+		glog.Error(context.Background(), "初始化邮件配置信息失败")
+		return
+	}
+	glog.Info(context.Background(), "邮件配置信息已加载成功")
+}
