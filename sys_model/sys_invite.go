f225284c244cb0b532cfac0f4426b09d699aab58
Arise <2778368047@qq.com>
2023-08-02 12:18:59 +0800

    style

--- sys_model/sys_invite.go
+++ sys_model/sys_invite.go
@@ -9,7 +9,7 @@ type Invite struct {
 	UserId         int64       `json:"userId"         description:"用户ID, 也就是邀约人ID" v:"required#邀约人ID不能为空"`
 	Value          string      `json:"value"          description:"邀约码背后的关联业务Json数据"  v:"json#验证信息必须为json格式字符串"`
 	ExpireAt       *gtime.Time `json:"expireAt"       description:"邀约码的过期失效" `
-	ActivateNumber int         `json:"activateNumber" description:"邀约码的激活次数限制" dc:"为0无上限"`
+	ActivateNumber int         `json:"activateNumber" description:"邀约码的激活次数限制" dc:"邀约码激活总次数"`
 	State          int         `json:"state"          description:"状态： 0失效、1正常" v:"required|in:0,1#邀约状态错误"`
 	Type           int         `json:"type"           description:"类型： 1注册、2加入团队、4加入角色 (复合类型)"`
 }
