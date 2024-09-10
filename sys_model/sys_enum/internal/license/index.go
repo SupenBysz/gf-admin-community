0a0ff8f64e37c06b4528cfb4667676d85a1c50d7
Arise <2778368047@qq.com>
2024-09-10 16:39:41 +0800

    feat: 暴露资质状态的枚举

--- sys_model/sys_enum/internal/license/index.go
+++ sys_model/sys_enum/internal/license/index.go
@@ -2,8 +2,10 @@ package sys_enum_license
 
 type license struct {
 	PermissionType permissionType
+	State          state
 }
 
 var License = license{
 	PermissionType: PermissionType,
+	State:          State,
 }
