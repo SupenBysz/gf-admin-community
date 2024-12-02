dff722ddb5e886db578979bcda60d89df3ba859b
SupenBysz <supen.huang@qq.com>
2024-12-03 07:54:55 +0800

    fix

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_casbin"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_category"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_config"
+	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_delivery_company"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_file"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_front_settings"
 	_ "github.com/SupenBysz/gf-admin-community/internal/logic/sys_industry"
