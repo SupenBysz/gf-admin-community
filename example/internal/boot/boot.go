384ea66463d5895dac5313c41f3d044801268058
SupenBysz <supen.huang@qq.com>
2024-12-20 15:24:46 +0800

    chore

--- example/internal/boot/boot.go
+++ example/internal/boot/boot.go
@@ -160,7 +160,7 @@ var (
 						group.Group("/industry", func(group *ghttp.RouterGroup) { group.Bind(sys_controller.SysIndustry) })
 						// 消息
 						group.Group("/message", func(group *ghttp.RouterGroup) { group.Bind(sys_controller.SysMessage) })
-						// 公告
+						// 公告管理
 						group.Group("/announcement", func(group *ghttp.RouterGroup) { group.Bind(sys_controller.SysAnnouncement) })
 						// 会员等级
 						group.Group("/memberLevel", func(group *ghttp.RouterGroup) { group.Bind(sys_controller.SysMemberLevel) })
