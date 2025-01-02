74cc81464c63efa170bdb69e26ab485a235b7b47
SupenBysz <supen.huang@qq.com>
2025-01-02 14:49:57 +0800

    chore

--- internal/boot/log_level_to_database.go
+++ internal/boot/log_level_to_database.go
@@ -2,11 +2,12 @@ package boot
 
 import (
 	"context"
-	"github.com/SupenBysz/gf-admin-community/sys_consts"
 	"github.com/gogf/gf/v2/container/garray"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/glog"
 	"github.com/gogf/gf/v2/text/gstr"
+
+	"github.com/SupenBysz/gf-admin-community/sys_consts"
 )
 
 // InitLogLevelToDatabase 加载日志写数据库的配置
