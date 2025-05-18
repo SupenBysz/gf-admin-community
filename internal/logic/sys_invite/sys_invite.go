cde8c645e135422942c2067897ec1aafd6236475
SupenBysz <supen.huang@qq.com>
2025-05-18 19:29:00 +0800

    fix(sys_invite): 优化 CreateInvite 方法中的邀约码处理逻辑
    - 在 CreateInvite 方法中，修改了对已存在邀约码的返回逻辑，确保返回邀约信息时调用 GetInviteById 方法，以提高代码的可读性和一致性。

--- internal/logic/sys_invite/sys_invite.go
+++ internal/logic/sys_invite/sys_invite.go
@@ -114,7 +114,7 @@ func (s *sSysInvite) CreateInvite(ctx context.Context, info *sys_model.Invite) (
 	// 判断该类型&该用户,是否已存在邀约码
 	invite, err := daoctl.ScanWithError[sys_model.InviteRes](sys_dao.SysInvite.Ctx(ctx).Where(sys_do.SysInvite{UserId: info.UserId, Type: info.Type}))
 	if invite != nil && invite.Id != 0 {
-		return invite, nil
+		return s.GetInviteById(ctx, invite.Id)
 	}
 
 	data := sys_do.SysInvite{}
