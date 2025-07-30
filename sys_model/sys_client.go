5f71e11343b8548bacfa983ba2ebf2e002e19795
SupenBysz <supen.huang@qq.com>
2025-07-30 10:13:30 +0800

    feat(sys_model): 添加用户类型白名单功能及检查方法
    新增用户类型白名单字段及检查方法，用于控制不同用户类型的访问权限

--- sys_model/sys_client.go
+++ sys_model/sys_client.go
@@ -2,6 +2,7 @@ package sys_model
 
 import (
 	"github.com/gogf/gf/v2/container/garray"
+	"github.com/kysion/base-library/utility/base_funs"
 )
 
 type ClientConfig struct {
@@ -16,4 +17,10 @@ type ClientConfig struct {
 	EnableRegisterInviteCode bool                   `json:"enableRegisterInviteCode"     dc:"注册是否需要邀请码"`
 	EnableSendCaptcha        bool                   `json:"enableSendCaptcha"     dc:"开启验证码发送"`
 	ApiPermissionWhitelist   []string               `json:"apiPermissionWhitelist" dc:"接口权限白名单，填写API路径（不含前缀接口前缀[apiPrefix中定义的迁移]），支持*为通配符，* 表示所有接口都不需要权限验证，*xxx 表示所有以xxx结尾的接口都不需要权限验证, xxx* 表示所有以xxx开头的接口都不需要权限验证"`
+	// 用户类型白名单，用户类型在 model/enum/user/type.go 中定义白名单中的用户类型将允许访问用户类型对应的额员工信息和公司信息
+	UserTypeWhitelist []int `json:"user_type_whitelist" dc:"用户类型白名单"`
+}
+
+func (c ClientConfig) HasUserTypeWhitelist(userType int) bool {
+	return base_funs.Contains(c.UserTypeWhitelist, userType)
 }
